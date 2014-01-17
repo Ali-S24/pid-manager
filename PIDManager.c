334674e82dfca5bfb940025e2397e3bf449dcf1a
Giancarlo Anemone <ganemone@gmail.com>
2014-01-17 16:47:25 -0500

    added support for variables to store min, max, and pid range

--- PIDManager.c
+++ PIDManager.c
@@ -4,23 +4,25 @@
 
 #include "PIDManager.h"
 
+
+
 int allocate_map() {
-	for(int i = 0; i < 4700; i++) {
+	for(int i = 0; i < MAX_PID; i++) {
 		pid_map[i] = 0;
 	}
 	return 1;
 }
 
 int allocate_pid() {
-	for(int i = 0; i < 4700; i++) {
+	for(int i = 0; i < MAX_PID; i++) {
 		if(pid_map[i] == 0) {
 			pid_map[i] = 1;
-			return i + 300;
+			return i + MIN_PID;
 		}
 	}
 	return 1;
 }
 
 void release_pid(int pid) {
-//	pid_map[pid + 300] = 0;
+	pid_map[MIN_PID + pid] = 0;
 }
--- PIDManager.h
+++ PIDManager.h
@@ -1,3 +1,6 @@
+int MAX_PID = 5000;
+int  MIN_PID = 500;
+int PID_RANGE = 4700;
 int pid_map[4700];
 
 int allocate_map();
